1f30ca88f60910ed4475cb89f220d540a4320ce1
Peterson Rodrigues dos Santos <petersonpepi16@gmail.com>
2025-05-06 21:17:15 -0300

    adicionar modal de fim de jogo e ajustar lógica de reinício no jogo da cobrinha, além disso, adicionar morte quando a cobra ultrapassa o propio corpo.

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="backgroundModal">
+    <div class="modalContent">
+      <h3>Hello world!</h3>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.backgroundModal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+
+.modalContent {
+  background: #212529;
+  width: 90%;
+  height: 15rem;
+  max-width: 500px;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  animation: fadeIn 0.3s ease-out;
+  padding: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/components/snakeGame.vue
+++ src/components/snakeGame.vue
@@ -1,15 +1,65 @@
 <template>
-  <h3>Snake game</h3>
-  <div class="headerContainer">
-    <RouterLink class="link" to="/">Voltar para a tela inicial</RouterLink>
-    <h3>score: {{ score }}</h3>
+  <main class="containerGame" v-if="!isOpenModalFimDeJogo">
+    <section v-if="jogoRodando">
+      <h3>Snake game</h3>
+      <div class="headerContainer">
+        <RouterLink class="link" to="/">Voltar para a tela inicial</RouterLink>
+        <h3>score: {{ score }}</h3>
+      </div>
+    </section>
+    <section v-else>
+      <button
+        style="margin-bottom: 10px"
+        class="btn btn-danger"
+        @click="reiniciarJogo"
+      >
+        Recomeçar o jogo
+      </button>
+    </section>
+    <canvas ref="gameCanvas"></canvas>
+
+    <section class="controls">
+      <button
+        class="btn btn-primary"
+        @click="() => changeDirection(null, 'ArrowUp')"
+      >
+        ↑
+      </button>
+
+      <button
+        class="btn btn-primary"
+        @click="() => changeDirection(null, 'ArrowLeft')"
+      >
+        ←
+      </button>
+
+      <button
+        class="btn btn-primary"
+        @click="() => changeDirection(null, 'ArrowRight')"
+      >
+        →
+      </button>
+
+      <button
+        class="btn btn-primary"
+        @click="() => changeDirection(null, 'ArrowDown')"
+      >
+        ↓
+      </button>
+    </section>
+  </main>
+
+  <div v-else>
+    <Modal />
   </div>
-  <canvas ref="gameCanvas" width="500" height="600"></canvas>
 </template>
 
 <script>
+import Modal from "./ModalComponent.vue";
+
 export default {
   name: "SnakeGame",
+  components: { Modal },
   data() {
     return {
       canvas: null,
@@ -20,9 +70,47 @@ export default {
       posicaoComidaY: null,
       calcularPosicaoComida: true,
       score: 0,
+      jogoRodando: true,
+      isOpenModalFimDeJogo: true,
     };
   },
   methods: {
+    mudarTamanhoCanvas() {
+      if (window.innerWidth <= 600) {
+        this.canvas.width = innerWidth / 1.5;
+        this.canvas.height = innerHeight / 2;
+      } else {
+        this.canvas.width = 500;
+        this.canvas.height = 600;
+      }
+      this.drawSnake();
+    },
+
+    gameOver() {
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+      // if (window.innerWidth < 600) {
+      //   this.ctx.fillStyle = "red";
+      //   this.ctx.font = "20px Comic Sans MS";
+      //   this.ctx.fillText("Game Over!", 150 / 3, 250 / 2);
+      //   this.ctx.fillStyle = "black";
+      //   this.ctx.font = "10px Comic Sans MS";
+      //   this.ctx.fillText("Pressione R para jogar novamente", 90 / 3, 300 / 2);
+      //   this.jogoRodando = false;
+      // } else {
+      //   this.ctx.fillStyle = "red";
+      //   this.ctx.font = "30px Comic Sans MS";
+      //   this.ctx.fillText("Game Over!", 150, 250);
+      //   this.ctx.fillStyle = "black";
+      //   this.ctx.font = "20px Comic Sans MS";
+      //   this.ctx.fillText("Pressione R para jogar novamente", 90, 300);
+      // }
+      this.isOpenModalFimDeJogo = true;
+      this.jogoRodando = false;
+    },
+    reiniciarJogo() {
+      window.location.reload();
+    },
+
     resetSnake(head) {
       this.snake.unshift(head);
       this.snake.pop();
@@ -42,14 +130,22 @@ export default {
       this.calcularPosicaoComidaMath();
 
       this.ctx.fillStyle = "red";
-      //prettier-ignore
-      this.ctx.fillRect(this.posicaoComidaX, this.posicaoComidaY, 30, 30);
+      this.ctx.fillRect(
+        this.posicaoComidaX,
+        this.posicaoComidaY,
+        window.innerWidth <= 600 ? 25 : 30,
+        window.innerWidth <= 600 ? 25 : 30
+      );
     },
     drawSnake() {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
       this.snake.forEach((segment) => {
         this.ctx.fillStyle = "green";
-        this.ctx.fillRect(segment.x, segment.y, 30, 30);
+        if (window.innerWidth <= 600) {
+          this.ctx.fillRect(segment.x / 2, segment.y / 2, 25, 25);
+        } else {
+          this.ctx.fillRect(segment.x, segment.y, 30, 30);
+        }
       });
 
       this.criarComida();
@@ -70,15 +166,44 @@ export default {
         head.x += this.blockSize;
       }
 
-      if (
-        head.x > this.canvas.width - 20 ||
-        head.x === -30 ||
-        head.y > this.canvas.height - 10 ||
-        head.y === -20
-      ) {
-        alert("Game Over");
-        window.location.reload();
+      // console.log(this.canvas.width + head.x - head.x);
+
+      // console.log(this.canvas.width);
+
+      // console.log(
+      //   "calculo",
+      //   this.canvas.width + window.innerWidth - window.innerWidth
+      // );
+
+      if (innerWidth <= 600) {
+        if (
+          // head.x > this.canvas.width - 20 ||
+          head.x === -30 ||
+          // head.y > this.canvas.height - 10 ||
+          head.y === -20
+        ) {
+          this.gameOver();
+          return;
+        }
+      } else {
+        if (
+          head.x > this.canvas.width - 20 ||
+          head.x === -30 ||
+          head.y > this.canvas.height - 10 ||
+          head.y === -20
+        ) {
+          this.gameOver();
+          return;
+        }
       }
+
+      this.snake.map((item, index) => {
+        //prettier-ignore
+        if (head.x === item.x && head.y === item.y && index !== 0) {
+          this.gameOver();
+          return;
+        }
+      });
 
       if (
         //prettier-ignore
@@ -92,21 +217,31 @@ export default {
 
       this.resetSnake(head);
     },
-    changeDirection(event) {
-      const key = event.key;
-      this.direction = key;
-      this.moveSnake();
-    },
-    update() {
-      this.drawSnake();
+    changeDirection(event, button) {
+      const key = button || event.key;
+      if (this.jogoRodando && (key !== "r" || key !== "R")) {
+        this.direction = key;
+        this.moveSnake();
+      }
+
+      //prettier-ignore
+      if (this.jogoRodando === false && (key === "r" || key === "R")) this.reiniciarJogo();
+
+      // if (!this.jogoRodando && (key !== "r" || key !== "R")) {
+      //   alert("Você perdeu! Tente novamente clicando no botão de reiniciar.");
+      // }
     },
   },
   mounted() {
     this.canvas = this.$refs.gameCanvas;
-    this.ctx = this.canvas.getContext("2d");
-    window.addEventListener("keydown", this.changeDirection);
-    this.update();
-    this.criarComida();
+
+    if (this.canvas) {
+      this.ctx = this.canvas.getContext("2d");
+      this.mudarTamanhoCanvas();
+      window.addEventListener("resize", this.mudarTamanhoCanvas);
+      window.addEventListener("keydown", this.changeDirection);
+      this.drawSnake();
+    }
   },
   beforeDestroy() {
     window.removeEventListener("keydown", this.changeDirection);
@@ -136,5 +271,43 @@ canvas {
   background-color: #0bd644;
   padding: 7px;
   border-radius: 20px;
+}
+
+.controls {
+  display: none;
+  /* display: flex;
+  flex-direction: row;
+  gap: 10px;
+  justify-content: center; */
+}
+
+@media (max-width: 600px) {
+  .containerGame {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: calc(100dvw - 50px);
+    height: calc(100dvh - 50px);
+    transform: translate(-50%, -50%);
+  }
+}
+@media (max-width: 400px) {
+  .controls {
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+    justify-content: center;
+  }
+
+  canvas {
+    background-color: white;
+    margin-bottom: 10px;
+  }
+
+  .headerContainer {
+    flex-direction: column;
+    gap: 0px;
+  }
 }
 </style>
